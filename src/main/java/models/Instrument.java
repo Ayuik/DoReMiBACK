5290a8c7f7e7003180042be201addbf8a28da277
Ayuik <marcosayelen@gmail.com>
2025-02-23 10:52:11 +0100

    FEAT [SCRUM-19]InstrumentModel refactored

--- src/main/java/models/Instrument.java
+++ src/main/java/models/Instrument.java
@@ -1,31 +1,11 @@
 package models;
 
 public class Instrument {
-    public String makeSoundDo(){
-        return enums.EnumInstrumentNotes.DO.getNoteName();
+    enums.EnumInstrumentNotes note;
+    String  sound = note.getNoteName();
+
+    public String makeSound(enums.EnumInstrumentNotes note){
+        return sound;
     }
 
-    public String makeSoundRe(){
-        return enums.EnumInstrumentNotes.RE.getNoteName();
-    }
-
-    public String makeSoundMi(){
-        return enums.EnumInstrumentNotes.MI.getNoteName();
-    }
-
-    public String makeSoundFa(){
-        return enums.EnumInstrumentNotes.FA.getNoteName();
-    }
-
-    public String makeSoundSol(){
-        return enums.EnumInstrumentNotes.SOL.getNoteName();
-    }
-
-    public String makeSoundLa(){    
-        return enums.EnumInstrumentNotes.LA.getNoteName();
-    }
-
-    public String makeSoundSi(){
-        return enums.EnumInstrumentNotes.SI.getNoteName();
-    }   
 }
