50543c2b0575b02a273743d7d2f1a35c93e482b5
Algama17 <albertogarciamallada@gmail.com>
2025-02-24 14:34:14 +0100

    create DatabaseConnection test

--- src/main/java/DatabaseConnection/DatabaseConnection.java
+++ src/main/java/DatabaseConnection/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:h2:./:testdb";
+    private static final String URL = "jdbc:h2:./testdb";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
