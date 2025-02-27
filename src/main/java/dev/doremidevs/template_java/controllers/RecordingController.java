476210196f9bb23abf672e51fa4ac9dceb1702d6
Emagmunioz <emagmunioz@gmail.com>
2025-02-27 15:52:01 +0100

    recordingController WIP

--- src/main/java/dev/doremidevs/template_java/controllers/RecordingController.java
+++ src/main/java/dev/doremidevs/template_java/controllers/RecordingController.java
@@ -3,22 +3,28 @@ package dev.doremidevs.template_java.controllers;
 import dev.doremidevs.template_java.models.RecordingModel;
 import java.util.ArrayList;
 
+import DatabaseConnection.RecordingRepository;
+
 public class RecordingController {
 
+    private RecordingRepository recordingRepository = new RecordingRepository();
+
     public ArrayList<RecordingModel> index() {
-        return null;
+        return recordingRepository.getRecordings();
 
     }
 
     public void create(RecordingModel recordingModel) {
-
+        RecordingModel recording = new RecordingModel(recordingName, recording);
+        recordingRepository.saveRecording(recording);
     }
 
     public RecordingModel show(String recordingName) {
-        return null;
+        return recordingRepository.getRecordingByName(recordingName);
     }
 
     public void destroy(String recordingName) {
+        recordingRepository.deleteRecording(recordingName);
     }
 
 }
