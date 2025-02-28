5f3c3a706afa5007205f91601cec4fc76750ae34
Ayuik <marcosayelen@gmail.com>
2025-02-28 14:45:58 +0100

    FEAT trying api connectiong. dependencies modified

--- src/main/java/dev/doremidevs/template_java/utils/App.java
+++ src/main/java/dev/doremidevs/template_java/utils/App.java
@@ -1,9 +1,30 @@
 package dev.doremidevs.template_java.utils;
-public final class App {
-    private App() {}
 
-    public static void main(String[] args) {
-        System.out.println("My Java Template");
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import javax.servlet.Servlet;
 
+import DatabaseConnection.RecordingServlet;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        handler.setContextPath("/");
+        server.setHandler(handler);
+
+        // Crear una instancia del ServletHolder
+        ServletHolder servletHolder = new ServletHolder();
+        servletHolder.setServlet((Servlet) new RecordingServlet());
+        handler.addServlet(servletHolder, "/mi-servlet");
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
